03aecb215d1b5130787be13032d309f5024f356a
dao1oad <86087622+dao1oad@users.noreply.github.com>
2026-03-20 21:38:28 +0800

    feat: add binance recovery backoff

--- src/perp_platform/runtime/binance/__init__.py
+++ src/perp_platform/runtime/binance/__init__.py
@@ -7,15 +7,27 @@ from .bootstrap import (
 )
 from .clients import BinanceExecutionClient, BinanceStreamClient
 from .config import BinanceRuntimeConfig, load_binance_runtime_config
+from .recovery import (
+    BinanceRecoveryEvent,
+    BinanceRecoveryPhase,
+    BinanceRecoveryState,
+    advance_binance_recovery,
+    begin_binance_recovery,
+)
 from .runtime import BinanceRuntimeWiring, wire_binance_runtime
 
 __all__ = [
     "BinanceClientTargets",
     "BinanceExecutionClient",
+    "BinanceRecoveryEvent",
+    "BinanceRecoveryPhase",
+    "BinanceRecoveryState",
     "BinanceRuntimeBootstrapResult",
     "BinanceRuntimeConfig",
     "BinanceRuntimeWiring",
     "BinanceStreamClient",
+    "advance_binance_recovery",
+    "begin_binance_recovery",
     "bootstrap_binance_runtime",
     "load_binance_runtime_config",
     "wire_binance_runtime",
